7e61224f712aba8a03d7c8c0faf57d22baaa81f3
justice0910 <ryanthackson@gmail.com>
2025-06-03 02:47:10 -0400

    fix: change relevant tailwindcss

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,22 +1,42 @@
+import {useState} from "react"
 import Divide from "@/components/utils/Divide"
+import ChatBox from "@/components/menu/ChatBox";
+
+
+export interface IChat {
+  id: number;
+}
+
 
 function Menu() {
+  const [chats, setChats] = useState<IChat[]>([])
+
+  const handlerAddChat = () => {
+    setChats(prevState => [...prevState, {id: Math.random()}])
+  }
 
   return (
     <div className={'flex flex-col justify-between w-64 bg-menuColors-800 p-2'}>
       {/*Top*/}
       <div className={'mb-4'}>
-        <button className={'p-2 border rounded-lg mb-2 w-full text-left text-white'}>+ New chat</button>
+        <button
+          className={'p-2 border rounded-lg mb-2 w-full text-left text-white'}
+          onClick={handlerAddChat}
+        >+ New chat</button>
         <input
           type="text"
           placeholder={'Search...'}
-          className={'border border-gray-300 p-1 rounded-l w-full focus-visible:outline-none'}
+          className={'border border-gray-300 p-2 rounded-lg w-full bg-menuColors-700 focus: text-white'}
         />
       </div>
 
       {/*Content*/}
       <Divide/>
-      <div className={'flex-grow'}></div>
+      <div className={'flex-grow overflow-auto scrollable'}>
+        {chats.map(chat => (
+          <ChatBox key={chat.id} id={chat.id} chatsArr={chats} setChatsArr={setChats}/>
+        ))}
+      </div>
 
       {/*Button*/}
       <Divide/>
